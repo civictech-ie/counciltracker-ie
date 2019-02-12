6d009d45e8fb0c2c57abbe0f052852bc8b7b5962
Brian Flanagan <brian@elbow.ie>
2019-02-12 08:50:06 +0000

    spec validates seat for attendance

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -3,4 +3,21 @@ require 'rails_helper'
 RSpec.describe Attendance, type: :model do
   it { should validate_presence_of(:councillor) }
   it { should validate_presence_of(:status) }
+
+  it "should be invalid if the councillor is not on the council" do
+    council_session = build(:council_session, commenced_on: 1.week.ago.to_date)
+    councillor = build(:councillor)
+    meeting = build(:meeting, occurred_on: Date.current)
+    attendance = build(:attendance, attendable: meeting, councillor: councillor)
+    expect(attendance.valid?).to be false
+  end
+
+  it "should be valid if the councillor is on the council" do
+    council_session = build(:council_session, commenced_on: 1.week.ago.to_date)
+    councillor = build(:councillor)
+    seat = create(:seat, councillor: councillor, commenced_on: 1.week.ago.to_date)
+    meeting = build(:meeting, occurred_on: Date.current)
+    attendance = build(:attendance, attendable: meeting, councillor: councillor)
+    expect(attendance.valid?).to be true
+  end
 end
