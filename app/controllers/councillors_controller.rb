c316912e8cba6b43558cd7ccba52ac2d1463b3cb
Brian Flanagan <brian@elbow.ie>
2019-06-17 22:17:08 +0100

    ibid for votes

--- app/controllers/councillors_controller.rb
+++ app/controllers/councillors_controller.rb
@@ -6,8 +6,8 @@ class CouncillorsController < ApplicationController
 
   def show
     @councillor = Councillor.find_by(slug: params[:id])
-    @proposed_motions = @councillor.proposed_motions.published.by_occurred_on
-    @proposed_amendments = @councillor.proposed_amendments.published.by_occurred_on
+    @proposed_motions = @councillor.proposed_motions.published.by_occurred_on.includes(:votes)
+    @proposed_amendments = @councillor.proposed_amendments.published.by_occurred_on.includes(:votes)
     @seat = @councillor.seat
     @attendances = @councillor.attendances.countable.includes(:attendable)
     @votes = @councillor.votes.countable.sort_by(&:occurred_on).reverse
