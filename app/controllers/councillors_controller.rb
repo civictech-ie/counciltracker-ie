2bd972ca7c8dfd72e14163412581c1e14db7b0f3
Brian Flanagan <brian@elbow.ie>
2019-06-17 22:15:42 +0100

    rm n+1 from councillors

--- app/controllers/councillors_controller.rb
+++ app/controllers/councillors_controller.rb
@@ -9,7 +9,7 @@ class CouncillorsController < ApplicationController
     @proposed_motions = @councillor.proposed_motions.published.by_occurred_on
     @proposed_amendments = @councillor.proposed_amendments.published.by_occurred_on
     @seat = @councillor.seat
-    @attendances = @councillor.attendances.countable
+    @attendances = @councillor.attendances.countable.includes(:attendable)
     @votes = @councillor.votes.countable.sort_by(&:occurred_on).reverse
   end
 end
