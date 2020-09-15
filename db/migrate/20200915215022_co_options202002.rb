1ca4c7394e885b882a38d9cb164c94ff0b9a1a07
Brian Flanagan <brian@elbow.ie>
2020-09-15 21:43:47 +0100

    update

--- db/migrate/20200915215022_co_options202002.rb
+++ db/migrate/20200915215022_co_options202002.rb
@@ -3,21 +3,21 @@ class CoOptions202002 < ActiveRecord::Migration[6.0]
     date = Date.new(2020,7,1)
     session = CouncilSession.current_on(date).take
 
-    CoOption.create(
+    CoOption.create!(
       occurred_on: date,
       outgoing_seat: session.seats.find_by_councillor_name("Marie Sherlock"),
       incoming_councillor: Councillor.find_or_create_by(full_name: "Declan Meenagh"),
       incoming_party: Party.find_by(slug: "labour")
     )
 
-    CoOption.create(
+    CoOption.create!(
       occurred_on: date,
       outgoing_seat: session.seats.find_by_councillor_name("Lawrence Hemmings"),
       incoming_councillor: Councillor.find_or_create_by(full_name: "Dearbháil Butler"),
       incoming_party: Party.find_by(slug: "green")
     )
 
-    CoOption.create(
+    CoOption.create!(
       occurred_on: date,
       outgoing_seat: session.seats.find_by_councillor_name("Rebecca Moynihan"),
       incoming_councillor: Councillor.find_or_create_by(full_name: "Darragh Moriarty"),
