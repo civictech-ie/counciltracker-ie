ed09f770195f28f5e0171d7687bee1cd3c464d9c
Brian Flanagan <brian@elbow.ie>
2020-09-15 21:49:14 +0100

    fix party slugs

--- db/migrate/20200915204726_co_options_june2020.rb
+++ db/migrate/20200915204726_co_options_june2020.rb
@@ -1,4 +1,4 @@
-class CoOptions202002 < ActiveRecord::Migration[6.0]
+class CoOptionsJune2020 < ActiveRecord::Migration[6.0]
   def change
     date = Date.new(2020,7,1)
     session = CouncilSession.current_on(date).take
@@ -6,22 +6,22 @@ class CoOptions202002 < ActiveRecord::Migration[6.0]
     CoOption.create!(
       occurred_on: date,
       outgoing_seat: session.seats.find_by_councillor_name("Marie Sherlock"),
-      incoming_councillor: Councillor.find_or_create_by(full_name: "Declan Meenagh"),
-      incoming_party: Party.find_by(slug: "labour")
+      incoming_councillor: Councillor.find_or_create_by!(full_name: "Declan Meenagh"),
+      incoming_party: Party.find_by!(slug: "labour-party")
     )
 
     CoOption.create!(
       occurred_on: date,
       outgoing_seat: session.seats.find_by_councillor_name("Lawrence Hemmings"),
-      incoming_councillor: Councillor.find_or_create_by(full_name: "Dearbháil Butler"),
-      incoming_party: Party.find_by(slug: "green")
+      incoming_councillor: Councillor.find_or_create_by!(full_name: "Dearbháil Butler"),
+      incoming_party: Party.find_by!(slug: "green-party")
     )
 
     CoOption.create!(
       occurred_on: date,
       outgoing_seat: session.seats.find_by_councillor_name("Rebecca Moynihan"),
-      incoming_councillor: Councillor.find_or_create_by(full_name: "Darragh Moriarty"),
-      incoming_party: Party.find_by(slug: "labour")
+      incoming_councillor: Councillor.find_or_create_by!(full_name: "Darragh Moriarty"),
+      incoming_party: Party.find_by!(slug: "labour-party")
     )
   end
 end
