21b8f94a91c2e5dd4d64993c144453a0407747d8
Brian Flanagan <brian@elbow.ie>
2020-11-17 18:34:13 +0000

    fix bug

--- db/migrate/20201117181814_co_options_oct2020.rb
+++ db/migrate/20201117181814_co_options_oct2020.rb
@@ -10,8 +10,7 @@ class CoOptionsOct2020 < ActiveRecord::Migration[6.0]
     )
     CoOption.create(
       occurred_on: date,
-      outgoing_seat: session.seats.find_by_councillor_name("Críona Ní Dhálaigh
-"),
+      outgoing_seat: session.seats.find_by_councillor_name("Críona Ní Dhálaigh"),
       incoming_councillor: Councillor.find_or_create_by(full_name: "Máire Devine", dcc_id: "961"),
       incoming_party: Party.find_by(slug: "sinn-fein")
     )
