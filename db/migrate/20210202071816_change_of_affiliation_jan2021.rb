136663ae656cad28f49be27031918f5fd410247c
Brian Flanagan <brian@elbow.ie>
2021-02-02 07:47:04 +0000

    fix sophie n affiliation event

--- db/migrate/20210202071816_change_of_affiliation_jan2021.rb
+++ db/migrate/20210202071816_change_of_affiliation_jan2021.rb
@@ -1,11 +1,10 @@
 class ChangeOfAffiliationJan2021 < ActiveRecord::Migration[6.1]
   def change
     date = Date.new(2021, 1, 15)
-    session = CouncilSession.current_on(date).take
     
-    ChangeOfAffiliation.create(
+    ChangeOfAffiliation.create!(
       occurred_on: date,
-      councillor: session.seats.find_by_councillor_name("sophie-nicoullaud"),
+      councillor: Councillor.find_by(slug: "sophie-nicoullaud"),
       outgoing_party: Party.find_by(slug: "green-party"),
       incoming_party: Party.find_by(slug: "independent")
     )
